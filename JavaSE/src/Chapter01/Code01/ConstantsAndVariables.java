a709c70969bae73cf1c6cfb8a9a3662d14bf8a27
Arry <2602340377@qq.com>
2020-10-08 16:42:52 +0800

    搭建项目库结构

---
+++ JavaSE/src/Chapter01/Code01/ConstantsAndVariables.java
@@ -0,0 +1,14 @@
+package Chapter01.Code01;
+
+/**
+ * @author: ArryArry
+ * @description: JavaSE基础语法之常量和变量
+ * @data: 2020-10-08 16:38
+ **/ 
+public class ConstantsAndVariables {
+    public static void main(String[] args) {
+        System.out.println("");
+
+
+    }
+}
